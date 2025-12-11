e854558bf6cfbe1c4bc4352570e9a4a6d40e9a2b
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2025-12-12 00:20:36 +0900

    Feat(auth-api): Auth 정보 관련을 다루는 API 구현 ## Feat - /login : 로그인을 담당하며 성공 시, 세션을 등록합니다. - /logout : 로그아웃을 담당하며 세션을 파기합니다. - /signup : 회원가입을 담당합니다. - /me : 쿠키 정보와 세션 정보를 비교하여 내 정보를 불러옵니다. 자동 로그인에 사용될 수 있습니다.

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { type CipherKey } from "crypto";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// Express
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+
+// MongoDB
+import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
+
+// Routes
+import auth from "@/routes/auth";
+
+// .env
+const { PORT, MONGO_URI, SESSION_SECRET } = process.env;
+
+// MongoDB connection
+mongoose.set("strictQuery", true);
+mongoose.connect(MONGO_URI as string);
+
+// Session
+const initSession = session({
+  secret: SESSION_SECRET as CipherKey,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: MONGO_URI as string }),
+  cookie: { httpOnly: true, secure: false, sameSite: "lax", maxAge: 1000 * 60 * 60 * 24 },
+});
+
+const app = express();
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Register
+app.use(initSession);
+
+// Router
+app.use(auth);
+
+app.listen(PORT, () => console.log(`Server online on port: ${PORT}`));
